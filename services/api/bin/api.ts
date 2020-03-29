cd30906027f12131c4487deb66624d667e0ad9a9
Alan Cooney <alan@skyhookadventure.com>
2020-03-29 11:14:54 +0100

    Add api service with schema only
    The infrastructure code will follow

--- services/api/bin/api.ts
+++ services/api/bin/api.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { LocationStoreStack } from '../lib/location-store-stack';
+import { ApiStack } from '../lib/api-stack';
 
 const app = new cdk.App();
-new LocationStoreStack(app, 'LocationStoreStack');
+new ApiStack(app, 'ApiStack');
--- services/api/test/api.test.ts
+++ services/api/test/api.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import LocationStore = require('../lib/location-store-stack');
+import Api = require('../lib/api-stack');
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new LocationStore.LocationStoreStack(app, 'MyTestStack');
+    const stack = new Api.ApiStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
