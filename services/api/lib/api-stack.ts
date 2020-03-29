e80792b2b40b674a12ad913378c70969e3ea316c
Alan Cooney <alan@skyhookadventure.com>
2020-03-29 11:24:43 +0100

    User pool & API infrastructure added

--- services/api/lib/api-stack.ts
+++ services/api/lib/api-stack.ts
@@ -1,9 +1,32 @@
 import * as cdk from '@aws-cdk/core';
+import { UserPool } from '@aws-cdk/aws-cognito';
+import { GraphQLApi, UserPoolDefaultAction, FieldLogLevel } from '@aws-cdk/aws-appsync';
 
 export class ApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // User Pool
+    const userPool = new UserPool(this, 'UserPool', {
+      userPoolName: "users"
+    });
+
+    // API
+    const api = new GraphQLApi(this, 'Api', {
+      name: `demoapi`,
+      logConfig: {
+        fieldLogLevel: FieldLogLevel.ALL,
+      },
+      authorizationConfig: {
+        defaultAuthorization: {
+          userPool,
+          defaultAction: UserPoolDefaultAction.ALLOW,
+        },
+
+      },
+      schemaDefinitionFile: './schema.graphql',
+    });
+
+
   }
 }
